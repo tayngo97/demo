dea05906070efee1072253b0709dfd7f51e6b2c2
tayngo97 <anhtayd27111997@gmail.com>
2022-06-07 11:45:36 +0700

    up

---
+++ EsmService/src/main/java/com/stdio/esm/exception/HandleException.java
@@ -0,0 +1,16 @@
+package com.stdio.esm.exception;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+/**
+ * @author AnhKhoa
+ * @since 19/05/2022 - 11:11
+ */
+@RestControllerAdvice
+public class HandleException {
+
+    @ExceptionHandler(value = {EsmException.class})
+    public ResponseEntity<?> esmException(Exception e) {
+        return ResponseEntity.ok(e.getMessage());
+    }
+}
